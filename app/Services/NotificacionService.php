40062d69f55091c2a2e713d5e02826fb65dd28bc
papijon83 <papijon83@gmail.com>
2025-08-15 02:38:21 -0600

    LOG 5

--- app/Services/NotificacionService.php
+++ app/Services/NotificacionService.php
@@ -11,8 +11,8 @@ class NotificacionService
     public static function notifica($request,$notificacion)
     {
        $token = $request->bearerToken();
-       Http::sso()->withToken($token)->post('/notifications',$notificacion);
-       Http::sso()->withToken($token)->post('/notifications/mail',$notificacion);
+       Http::sso()->withToken($token)->withOptions(["verify"=>false])->post('/notifications',$notificacion);
+       Http::sso()->withToken($token)->withOptions(["verify"=>false])->post('/notifications/mail',$notificacion);
     }
 
 }
